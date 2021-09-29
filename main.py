dcfce023f34bddc292dddce89d8129e977242329
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-29 09:38:10 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function
+#add parameters
+def shareFair(x, y):
+    if x % y == 0:
+        return True
+    else:
+        return False
+#run function
+print(shareFair(10,6))
